abc8c0b731843ff7b4ce606558a71245e01a61d5
AlexanderT <alptyktsk@gmail.com>
2021-08-31 11:44:36 +0900

    initial commit

---
+++ src/main/java/com/baken/teach/shedulingtasks/Application.java
@@ -0,0 +1,13 @@
+package com.baken.teach.shedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
